b4c0e74cdc43331d70ce7d944e79ddbcd9031b66
taxshi792 <taxshi@gmail.com>
2021-04-14 02:04:10 +0000

    配列についての学習

---
+++ src/array.ts
@@ -0,0 +1,8 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings: string[] = ["tokyo", "osaka", "kyoto"];
+let strings2: Array<string> = ["tokyo", "osaka", "kyoto"];
+
