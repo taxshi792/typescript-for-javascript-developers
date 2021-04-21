415f11ac2bfab722d9bbdf6c9362411e861902a6
taxshi792 <taxshi@gmail.com>
2021-04-21 11:03:53 +0000

    リテラル型（保存ミス）

--- src/literal.ts
+++ src/literal.ts
@@ -1,4 +1,19 @@
 export {}
 
-let dayOfTheWeek = "日"
+let dayOfTheWeek: string = "日"
+dayOfTheWeek = "月"
+dayOfTheWeek = "火"
+dayOfTheWeek = "1976/01/22"  // 曜日以外も受け付けてしまう
 
+let dayOfTheWeek2: "日" | "月" | "火" | "水" | "木" | "金" | "土" = "日"
+dayOfTheWeek2 = "月"
+dayOfTheWeek2 = "火"
+dayOfTheWeek2 = "1976/01/22"  // エラーで弾ける
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1
+month = 12
+month = 100  // エラーで弾ける
+
+// booleanもリテラル型にできるが、どんな使い道があるのか…？
+let TRUE: true = true
+TRUE = false  // エラーで弾ける
